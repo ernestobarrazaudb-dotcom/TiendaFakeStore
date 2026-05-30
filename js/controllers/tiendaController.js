2dd656a42cafc5031d994e88dce8370bccddd369
ernestobarrazaudb-dotcom <ernestobarraza.udb@gmail.com>
2026-05-30 08:18:55 -0600

    Se agrega documentación a los diferentes archivos del sitio

--- js/controllers/tiendaController.js
+++ js/controllers/tiendaController.js
@@ -1,17 +1,19 @@
-app.controller('TiendaController', ['$scope', 'tiendaService', function($scope, tiendaService) {
+// Controlador para la tienda
+app.controller('TiendaController', ['$scope', 'tiendaService', function ($scope, tiendaService) {
     $scope.productos = [];
     $scope.categorias = [];
     
     $scope.searchText = '';
     $scope.selectedCategory = '';
 
-    
+    // Función para filtrar productos por categoría
     tiendaService.obtenerProductos().then(function(data) {
         $scope.productos = data;
     }).catch(function(error) {
         console.error("Error al cargar productos:", error);
     });
 
+    // Función para cargar categorías
     tiendaService.obtenerCategorias().then(function(data) {
         $scope.categorias = data;
     }).catch(function(error) {
